abd14a0463eb4733c265876daf86a04444ba72bd
Taphiriel <taphiriel@googlemail.com>
2026-05-26 16:31:57 +0200

    Fix cursor alpha masks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { BootScene } from "./scenes/BootScene.js?v=cursor-polish-1";
-import { MenuScene } from "./scenes/MenuScene.js?v=cursor-polish-1";
-import { CharacterScene } from "./scenes/CharacterScene.js?v=cursor-polish-1";
-import { GameScene } from "./scenes/GameScene.js?v=cursor-polish-1";
+import { BootScene } from "./scenes/BootScene.js?v=cursor-alpha-fix-1";
+import { MenuScene } from "./scenes/MenuScene.js?v=cursor-alpha-fix-1";
+import { CharacterScene } from "./scenes/CharacterScene.js?v=cursor-alpha-fix-1";
+import { GameScene } from "./scenes/GameScene.js?v=cursor-alpha-fix-1";
 
 const config = {
   type: Phaser.AUTO,
